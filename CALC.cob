29634e2f624a0f24c72806c7afcbd33bba7a6f2e
natanboanafina <boanafina.natan@gmail.com>
2022-11-07 09:39:18 -0300

    Initial commit

---
+++ CALC.cob
@@ -0,0 +1,27 @@
+       IDENTIFICATION                  DIVISION.
+       PROGRAM-ID.CALC.
+      *=================================================================
+      *    AUTHOR: NATAN BOANAFINA
+      *    DATE: 07-11-2022
+      *    PURPOSE: LEARNING AND TRAINNING SOME COBOL PRINCIPLES BY
+      *    CREATING A SIMPLE CALCULATOR.
+      *=================================================================
+       ENVIRONMENT                     DIVISION.
+       CONFIGURATION                   SECTION.
+       SPECIAL-NAMES.
+           DECIMAL-POINT IS COMMA.
+       DATA                            DIVISION.
+       WORKING-STORAGE                 SECTION.
+
+       PROCEDURE                       DIVISION.
+       0001-MAIN                       SECTION.
+           PERFORM 0100-INITIALIZE.
+           PERFORM 0200-PROCESS.
+           PERFORM 0300-END.
+           STOP RUN.
+       0100-INITIALIZE                 SECTION.
+           DISPLAY "HELLO, WORLD!".
+       0200-PROCESS                    SECTION.
+           CONTINUE.
+       0300-END                        SECTION.
+           DISPLAY "======== END OF PROGRAM ========".
