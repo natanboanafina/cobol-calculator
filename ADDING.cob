c8724817619ba32157e72899f62f8dbd3cf2fb02
natanboanafina <boanafina.natan@gmail.com>
2022-11-07 12:44:31 -0300

    Module ADDING created

---
+++ ADDING.cob
@@ -0,0 +1,14 @@
+       IDENTIFICATION                  DIVISION.
+       PROGRAM-ID.ADDING.
+      *=================================================================
+      *    AUTHOR:  NATAN BOANAFINA
+      *    DATE:    07-11-2022
+      *    PURPOSE: SUM LOGIC.
+      *=================================================================
+       ENVIRONMENT                     DIVISION.
+       CONFIGURATION                   SECTION.
+       SPECIAL-NAMES.
+           DECIMAL-POINT IS COMMA.
+       DATA                            DIVISION.
+       WORKING-STORAGE                 SECTION.
+       PROCEDURE                       DIVISION.
