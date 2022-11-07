26d2aebb541c6b595a0d17aa85a52ccbc465a91d
natanboanafina <boanafina.natan@gmail.com>
2022-11-07 13:12:30 -0300

    ADDING.cob called

--- CALC.cob
+++ CALC.cob
@@ -14,16 +14,20 @@
        WORKING-STORAGE                 SECTION.
        COPY "#DATEVAR".
        77  WRK-EXT-DATE        PIC X(30).
-
+       77  WRK-ADDING-VAR      PIC 9(10)V99.
        PROCEDURE                       DIVISION.
        0001-MAIN                       SECTION.
+
            PERFORM 0100-INITIALIZE.
            PERFORM 0200-PROCESS.
            PERFORM 0300-END.
            STOP RUN.
        0100-INITIALIZE                 SECTION.
+       0110-DISPLAY-DATE.
            CALL "FULLDATE" USING WRK-EXT-DATE.
            DISPLAY WRK-EXT-DATE.
+       0120-INIT-SUM.
+           CALL "ADDING"   USING WRK-ADDING-VAR.
 
        0200-PROCESS                    SECTION.
            CONTINUE.
