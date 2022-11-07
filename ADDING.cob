0ff819cffdd65a9762417c6287c05a8caf0f8b21
natanboanafina <boanafina.natan@gmail.com>
2022-11-07 13:10:58 -0300

    Sum Logic Added

--- ADDING.cob
+++ ADDING.cob
@@ -11,4 +11,25 @@
            DECIMAL-POINT IS COMMA.
        DATA                            DIVISION.
        WORKING-STORAGE                 SECTION.
-       PROCEDURE                       DIVISION.
+       COPY "#MATHVAR".
+       77  WRK-TOTAL-ED        PIC Z.ZZZ.ZZZ.ZZZ,ZZ.
+       LINKAGE                         SECTION.
+       01  LNK-VAR.
+           05 LNK-RESULT   PIC 9(10)V99.
+
+       PROCEDURE                       DIVISION USING LNK-VAR.
+       0001-MAIN                       SECTION.
+           PERFORM 0100-INITIALIZE.
+           PERFORM 0200-PROCESS.
+           GOBACK.
+
+       0100-INITIALIZE                 SECTION.
+           DISPLAY "DIGITE O PRIMEIRO NUMERO: ".
+             ACCEPT WRK-FIRST-NUM.
+           DISPLAY "DIGITE O SEGUNDO NUMERO: ".
+             ACCEPT WRK-SECOND-NUM.
+
+       0200-PROCESS                    SECTION.
+           COMPUTE WRK-TOTAL-NUM = WRK-FIRST-NUM + WRK-SECOND-NUM.
+           MOVE WRK-TOTAL-NUM TO WRK-TOTAL-ED.
+           DISPLAY WRK-TOTAL-ED.
